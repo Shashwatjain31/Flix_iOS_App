1feb59e94c569e888c236f017db9757a89815f22
Shubham Patil <shubhampatil1612@icloud.com>
2019-02-13 16:50:50 -0800

    My Flix App Part 2

--- My_Flix_App/MovieDetailsViewController.swift
+++ My_Flix_App/MovieDetailsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MovieDetailsViewController: UIViewController {
 
@@ -15,8 +16,34 @@ class MovieDetailsViewController: UIViewController {
     
     @IBOutlet weak var backDropView: UIImageView!
     
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleLabel.text = movie["title"] as? String
+        synopsisLabel.text = movie["overview"] as? String
+        
+        titleLabel.sizeToFit()
+        synopsisLabel.sizeToFit()
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        posterView.af_setImage(withURL: posterUrl!)
+        
+        let backDropPath = movie["backdrop_path"] as! String
+        
+        
+        let backDropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backDropPath)
+        
+        backDropView.af_setImage(withURL: backDropUrl!)
 
         // Do any additional setup after loading the view.
     }
